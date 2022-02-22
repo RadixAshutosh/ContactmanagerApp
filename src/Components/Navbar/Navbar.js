e9f98d4fcf0a4c183e5d3fb0365e8331dcbe51bf
Radix Ashutosh <ashutoshw@tech-radix.com>
2022-02-22 13:06:26 +0530

    initial comment

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+let Navbar=()=>{
+
+    return(
+        <React.Fragment>
+         <nav className='navbar navbar-dark bg-dark text-white '>
+             <h2 className='offset-1'> <i class="fa fa-facebook-f text-primary px-2"></i>
+              Contact Manager
+            </h2>
+           
+         </nav>
+        </React.Fragment>
+    )
+}
+
+export default Navbar;
